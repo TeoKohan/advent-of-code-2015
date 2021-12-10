e641533a5610ab5394568acf7a123bb64718a863
Firewill <Teo.Kohan@gmail.com>
2021-12-10 04:14:26 -0300

    Solve day 8

---
+++ Day 8/santas-list.py
@@ -0,0 +1,18 @@
+I = [line.strip() for line in open('input')]
+
+U = [line[1:-1] for line in I]
+U = [line.replace('\\', '|') for line in U]
+U = [line.replace('||', 'B') for line in U]
+U = [line.replace('|"', 'A') for line in U]
+U = [line.replace('|x', 'H') for line in U]
+
+E = [line.replace('B', '||||') for line in U]
+E = [line.replace('A', '||||') for line in E]
+E = [line.replace('H', '|||') for line in E]
+
+character_number = sum([len(line) for line in I])
+decode_number    = sum([len(line) - line.count('H') * 2 for line in U])
+encode_number    = sum([len(line) + 6 for line in E])
+
+print(character_number - decode_number)
+print(encode_number - character_number)
